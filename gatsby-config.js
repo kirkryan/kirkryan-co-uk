3453066bfe86162b1ce1354676732cb20f7a0c8f
Kirk Ryan <kirkryan@users.noreply.github.com>
2021-10-25 11:27:09 +0100

    Update gatsby-config.js
    Modify site metadata

--- gatsby-config.js
+++ gatsby-config.js
@@ -36,8 +36,8 @@ if (!spaceId || !accessToken) {
 
 module.exports = {
   siteMetadata: {
-    title: "Gatsby Contentful Starter",
-    description: "Official Contentful Gatsby Starter",
+    title: "Kirk Ryan",
+    description: "Research and recommendations for technology start-ups",
   },
   pathPrefix: "/gatsby-contentful-starter",
   plugins: [
